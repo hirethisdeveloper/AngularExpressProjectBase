c2991b54840fe2b62703931ae03ab1f2a0e1cf78
Phil Pastorek <contact@hirethisdeveloper.com>
2015-10-18 14:58:09 -0400

    .

--- server/models/devices.js
+++
@@ -1,4 +0,0 @@
-var mysql                             = require("../utils/database"),
-    db                                = mysql.db,
-    uuid                              = require("node-uuid"),
-    bcrypt                            = require('bcrypt-nodejs');
