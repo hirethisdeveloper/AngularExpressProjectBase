a74571cacfde77179a5e26ce33bc696a8b921c8f
Phil Pastorek <contact@hirethisdeveloper.com>
2015-10-25 13:39:32 -0400

    updated structure.sql file

--- server/structure.sql
+++ server/structure.sql
@@ -7,7 +7,7 @@
 #
 # Host: 192.168.96.160 (MySQL 5.5.44-0ubuntu0.14.04.1)
 # Database: projectblockdb
-# Generation Time: 2015-10-23 20:01:41 +0000
+# Generation Time: 2015-10-25 17:39:26 +0000
 # ************************************************************
 
 
@@ -18,6 +18,38 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table _communicationTypes
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `_communicationTypes`;
+
+CREATE TABLE `_communicationTypes` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `guid` varchar(100) DEFAULT NULL,
+  `title` varchar(200) DEFAULT NULL,
+  `shortTitle` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table account_org_relation
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `account_org_relation`;
+
+CREATE TABLE `account_org_relation` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `guid` varchar(100) DEFAULT '',
+  `account_guid` varchar(100) DEFAULT NULL,
+  `org_guid` varchar(100) DEFAULT NULL,
+  `dateCreated` datetime DEFAULT NULL,
+  `org_level` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 
 # Dump of table accounts
@@ -32,6 +64,147 @@ CREATE TABLE `accounts` (
   `password` varchar(200) DEFAULT NULL,
   `dateCreated` datetime DEFAULT NULL,
   `status` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table api_accounts
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `api_accounts`;
+
+CREATE TABLE `api_accounts` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `guid` varchar(100) DEFAULT NULL,
+  `siteTitle` varchar(255) DEFAULT NULL,
+  `siteUrl` varchar(255) DEFAULT '',
+  `contactEmail` varchar(100) DEFAULT NULL,
+  `address1` varchar(255) DEFAULT NULL,
+  `address2` varchar(100) DEFAULT NULL,
+  `city` varchar(200) DEFAULT NULL,
+  `state` varchar(200) DEFAULT NULL,
+  `postalCode` varchar(50) DEFAULT NULL,
+  `status` tinyint(4) DEFAULT NULL,
+  `firstName` varchar(100) DEFAULT NULL,
+  `lastName` varchar(100) DEFAULT NULL,
+  `comments` longtext,
+  `remoteAddress` varchar(25) DEFAULT NULL,
+  `emailVerified` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='This table holds information on contacts that register for an API key. These api keys will be used to track and limit external communication into this application.';
+
+
+
+# Dump of table contactCommRelation
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `contactCommRelation`;
+
+CREATE TABLE `contactCommRelation` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `guid` varchar(100) DEFAULT NULL,
+  `org_guid` varchar(100) DEFAULT NULL,
+  `contact_guid` varchar(100) DEFAULT NULL,
+  `commType_guid` varchar(100) DEFAULT NULL,
+  `commValue` varchar(255) DEFAULT NULL,
+  `isDefault` tinyint(1) DEFAULT NULL,
+  `comment` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table contacts
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `contacts`;
+
+CREATE TABLE `contacts` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `guid` varchar(100) DEFAULT NULL,
+  `org_guid` varchar(100) DEFAULT NULL,
+  `location_guId` varchar(100) DEFAULT NULL,
+  `language_guid` varchar(100) DEFAULT NULL,
+  `firstName` varchar(100) DEFAULT NULL,
+  `lastName` varchar(100) DEFAULT NULL,
+  `comments` longtext,
+  `dateCreated` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table locations
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `locations`;
+
+CREATE TABLE `locations` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `guid` varchar(100) DEFAULT NULL,
+  `org_guid` varchar(100) DEFAULT NULL,
+  `contact_guId` varchar(100) DEFAULT NULL,
+  `address1` varchar(200) DEFAULT NULL,
+  `address2` varchar(200) DEFAULT NULL,
+  `city` varchar(100) DEFAULT NULL,
+  `state` varchar(100) DEFAULT NULL,
+  `country` varchar(100) DEFAULT NULL,
+  `comment` longtext,
+  `dateCreated` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table org_registration_meta
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `org_registration_meta`;
+
+CREATE TABLE `org_registration_meta` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `guid` varchar(100) DEFAULT NULL,
+  `org_guid` varchar(100) DEFAULT NULL,
+  `ipaddress` varchar(20) DEFAULT NULL,
+  `url` longtext,
+  `comments` longtext,
+  `emailAddress` varchar(100) DEFAULT NULL,
+  `api_guid` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table org_supported_languages
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `org_supported_languages`;
+
+CREATE TABLE `org_supported_languages` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `guid` varchar(100) DEFAULT NULL,
+  `org_guid` varchar(100) DEFAULT NULL,
+  `language_guid` varchar(100) DEFAULT NULL,
+  `isDefault` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table organizations
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `organizations`;
+
+CREATE TABLE `organizations` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `guid` varchar(100) DEFAULT NULL,
+  `title` varchar(200) DEFAULT NULL,
+  `shortTitle` varchar(100) DEFAULT NULL,
+  `location_guid` varchar(100) DEFAULT NULL,
+  `comments` longtext,
+  `dateCreated` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
